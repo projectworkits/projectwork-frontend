70556a735f05ad5a7d06070713814265568e1318
furio <furio.reini.25@stud.itsaltoadriatico.it>
2026-04-22 16:55:46 +0200

    04/22/2026 16:55:46 fix landing page

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -6,12 +6,12 @@ function renderFooter() {
   const year = new Date().getFullYear();
   const html = `
     <footer class="footer">
-      <a class="footer-brand" href="azienda.html">Silk &amp; Stack</a>
+      <a class="footer-brand" href="azienda>Silk &amp; Stack</a>
       <ul class="footer-links">
-        <li><a href="index.html">Home</a></li>
-        <li><a href="mostra-index.html">Galleria</a></li>
-        <li><a href="shop-index.html">Shop</a></li>
-        <li><a href="azienda.html">Azienda</a></li>
+        <li><a href="index>Home</a></li>
+        <li><a href="mostra-index>Galleria</a></li>
+        <li><a href="shop-index>Shop</a></li>
+        <li><a href="azienda>Azienda</a></li>
       </ul>
       <span>© ${year} Mostra Doisneau — Pordenone</span>
     </footer>
